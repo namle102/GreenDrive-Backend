ea1d7796b30ce3fa757f1d1a7179ddf0eccf8935
Nam Le <139657367+namle102@users.noreply.github.com>
2025-06-19 22:18:08 -0400

    Initial Spring Boot project added

---
+++ src/main/java/com/greendrive/backend/GreendriveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.greendrive.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreendriveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreendriveBackendApplication.class, args);
+	}
+
+}
