3b7f292046deb6e6d3d128cb34de7d466e086a70
Nam Le <139657367+namle102@users.noreply.github.com>
2025-06-19 22:18:08 -0400

    Initial Spring Boot project added

---
+++ src/test/java/com/greendrive/backend/GreendriveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greendrive.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreendriveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
